8cd2f2fdc81c82aa44fd733b083e868b4b02708f
Connorb731 <connorbriggs@pluginpromotions.com>
2025-05-25 13:33:14 -0400

    Delete bundle.js

--- bundle.js
+++
@@ -1,5 +0,0 @@
-// This is a production-ready JavaScript bundle
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Plug-In Promotions website loaded');
-    // Production JavaScript would be here
-});
